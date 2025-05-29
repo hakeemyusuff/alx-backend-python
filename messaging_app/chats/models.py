0009d0df8f1f2ef3615e9fb67530819875ea53d5
Yusuff Abdulhakeem <hakeemyusuff19@gmail.com>
2025-05-29 17:10:20 +0100

    Include missing fields in Users and Conversation

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -6,10 +6,14 @@ import uuid
 class User(AbstractUser):
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4())
     phone_number = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    password = models.CharField(max_length=128)
 
 
 class Conversation(models.Model):
-    Conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4())
+    conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4())
     participants = models.ManyToManyField(User, related_name="conversations")
 
 

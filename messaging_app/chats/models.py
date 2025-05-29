4ee358f05c99330b4d9f698614ede1bde4b0830e
Yusuff Abdulhakeem <hakeemyusuff19@gmail.com>
2025-05-29 17:04:28 +0100

    create Models for User, Conversations, Message

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
 
-# Create your models here.
+
+class User(AbstractUser):
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4())
+    phone_number = models.CharField(max_length=50)
+
+
+class Conversation(models.Model):
+    Conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4())
+    participants = models.ManyToManyField(User, related_name="conversations")
+
+
+class Message(models.Model):
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4())
+    message_body = models.TextField()
+    sent_at = models.TimeField(auto_now_add=True)
+    created_at = models.TimeField(auto_now_add=True)

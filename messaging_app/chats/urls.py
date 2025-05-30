71a7a4cafb484641043eba5eb6d24ef76c183b14
Yusuff Abdulhakeem <hakeemyusuff19@gmail.com>
2025-05-30 07:50:31 +0100

    creat urls for the views

---
+++ messaging_app/chats/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from .views import ConversationViewSet, UserViewSet, MessageViewSet
+
+router = DefaultRouter()
+router.register(r"conversations", ConversationViewSet, basename="conversations")
+router.register(r"messages", MessageViewSet, basename="messages")
+router.register(r"users", UserViewSet, basename="users")
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -2,14 +2,14 @@ from rest_framework import viewsets
 from .models import User, Conversation, Message
 from .serializers import UserSerializer, ConversationSerializer, MessageSerializer
 
-class UserViewsets(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     
-class ConversationViewset(viewsets.ModelViewSet):
+class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
     
-class MessageViewsets(viewsets.ModelViewSet):
+class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     

82755a7bdd0df5d662ac723dba92fadcd75c3b9a
Yusuff Abdulhakeem <hakeemyusuff19@gmail.com>
2025-05-30 08:43:24 +0100

    add router.urls to urls patterns

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
-from rest_framework_nested.routers import DefaultRouter
+from django.urls import path,include
+from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
 from .views import ConversationViewSet, UserViewSet, MessageViewSet
 
 router = DefaultRouter()
 router.register(r"conversations", ConversationViewSet, basename="conversations")
 router.register(r"messages", MessageViewSet, basename="messages")
 router.register(r"users", UserViewSet, basename="users")
-#api-auth
+conversation = NestedDefaultRouter(router, r"conversations", lookup="conversations")
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth')
+]

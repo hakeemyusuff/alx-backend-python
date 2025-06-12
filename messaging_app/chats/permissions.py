007dbd5ccd4514188620d3146c7ac8da4923760f
Yusuff Abdulhakeem <hakeemyusuff19@gmail.com>
2025-06-12 02:12:11 +0100

    add permission and implemen view

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,17 +1,31 @@
 from rest_framework import permissions
 from .models import Message, Conversation
 
+
 class IsSenderOrReceiver(permissions.BasePermission):
-    """Allows access only to users who are sender or receiver of a message.
-    """
-    
+    """Allows access only to users who are sender or receiver of a message."""
+
     def has_object_permission(self, request, view, obj):
         return obj.sender == request.user or obj.receiver == request.user
-    
+
 
 class IsParcticipant(permissions.BasePermission):
-    """Allows access to only participants that is part of the conversation.
-    """
-    
+    """Allows access to only participants that is part of the conversation."""
+
     def has_object_permission(self, request, view, obj):
         return request.user in obj.particpants.all()
+
+
+class IsParticipantOfConversation(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        conversation = getattr(obj, "converstation", None)
+        User = request.user
+
+        if conversation and User in conversation.participants.all():
+            if request.method in ["PUT", "PATCH", "DELETE", "POST", "GET"]:
+                return True
+
+        return False
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,8 +1,11 @@
-from rest_framework import viewsets
+from rest_framework import viewsets,status, filters
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from .models import User, Conversation, Message
 from .serializers import UserSerializer, ConversationSerializer, MessageSerializer
-from rest_framework import status
-from rest_framework import filters
+from .permissions import IsParticipantOfConversation
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -13,5 +16,23 @@ class ConversationViewSet(viewsets.ModelViewSet):
     serializer_class = ConversationSerializer
     
 class MessageViewSet(viewsets.ModelViewSet):
-    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated, IsParticipantOfConversation]
+    
+    def get_queryset(self):
+        conversation_id = self.kwargs.get("conversation_id")
+        conversation = get_object_or_404(Conversation, id=conversation_id)        
+        
+        if self.request.user not in conversation.participants.all():
+            return Message.objects.none()
+        
+        return Message.objects.filter(conversation=conversation)
+    
+    def perform_create(self, serializer):
+        conversation_id = self.kwargs.get("conversation_id")
+        conversation = get_object_or_404(Conversation, id=conversation_id)
+        
+        if self.request.user not in conversation.participants.all():
+            return Response({"default": "You're not a participant in this conversation."}, status=status.HTTP_403_FORBIDDEN)
+        serializer.save(sender=self.request.user, conversation=conversation)            
     

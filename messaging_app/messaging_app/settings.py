4d6bbe67dd9b2fcf06f2b3e94f818da578f873f7
Yusuff Abdulhakeem <hakeemyusuff19@gmail.com>
2025-06-12 02:42:38 +0100

    fix pagination checker error

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -134,7 +134,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ],
-    "DEFAULT_PAGINATION_CLASS": "chats.pagination.MessagePagination",
+    "DEFAULT_PAGINATION_CLASS": "chats.pagination.MessagePagination",  # Inherits from PageNumberPagination
     "PAGE_SIZE": 20,
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
 }

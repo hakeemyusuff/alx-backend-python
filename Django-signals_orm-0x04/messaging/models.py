ca012a7c5fe265f09688a2895b46803bd52db685
Yusuff Abdulhakeem <hakeemyusuff19@gmail.com>
2025-06-15 17:40:47 +0100

    add stuff to views

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -16,9 +16,20 @@ class Message(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     edited = models.BooleanField(default=False)
+    
+    parent_message = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        related_name="replies",
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
         return f"from {self.sender.first_name} to {self.receiver.first_name} at {self.timestamp}"
+    
+    def is_reply(self):
+        return self.parent_message is not None
 
 
 class Notification(models.Model):
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+
+from .models import Message
 
 @login_required
 def delete_user(request):
     user = request.user
     user.delete()
     return redirect('home')
+
+@login_required
+def send_message(request, receiver_id):
+    receiver = get_object_or_404(User, id=receiver_id)
+    
+    if request.method == 'POST':
+        content = request.POST.get("content")
+        parent_id = request.POST.get("parent_id")
+        parent_message = Message.objects.filter(id=parent_id).first() if parent_id else None
+        
+        Message.objects.create(
+            sender = request.user,
+            receiver= receiver,
+            content=content,
+            parent_message=parent_message,
+        )
+        return redirect('inbox')
+    
+    return

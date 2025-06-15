c8f96aeea90945197cb047675c18079e92c03474
Yusuff Abdulhakeem <hakeemyusuff19@gmail.com>
2025-06-15 16:54:40 +0100

    write signal for notification

--- Django-signals_orm-0x04/messaging/apps.py
+++ Django-signals_orm-0x04/messaging/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
+import messaging
 
 
 class MessagingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'messaging'
+    
+    def ready(self):
+        return messaging.signals

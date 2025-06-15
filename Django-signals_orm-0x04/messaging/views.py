bd0fd3d549c850bbf19d039eb26602f140f0007d
Yusuff Abdulhakeem <hakeemyusuff19@gmail.com>
2025-06-15 17:43:33 +0100

    add stuff to views

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -12,12 +12,12 @@ def delete_user(request):
 @login_required
 def send_message(request, receiver_id):
     receiver = get_object_or_404(User, id=receiver_id)
-    
+
     if request.method == 'POST':
         content = request.POST.get("content")
         parent_id = request.POST.get("parent_id")
         parent_message = Message.objects.filter(id=parent_id).first() if parent_id else None
-        
+
         Message.objects.create(
             sender = request.user,
             receiver= receiver,
@@ -25,5 +25,24 @@ def send_message(request, receiver_id):
             parent_message=parent_message,
         )
         return redirect('inbox')
-    
+
+    return
+
+
+def get_threaded_replies(message):
+    """Recursively get all replies to a message"""
+    replies = []
+    direct_replies = message.replies.all().select_related("sender", "receiver")
+    for reply in direct_replies:
+        replies.append(reply)
+        replies.extend(get_threaded_replies(reply))
+    return replies
+
+
+@login_required
+def view_thread(request, message_id):
+    message = get_object_or_404(
+        Message.objects.select_related("sender", "receiver"), id=message_id
+    )
+    replies = get_threaded_replies(message)
     return

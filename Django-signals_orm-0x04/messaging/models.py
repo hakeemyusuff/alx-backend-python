2e6c5027304a33d68e5419a01bf6a6685a323c58
Yusuff Abdulhakeem <hakeemyusuff19@gmail.com>
2025-06-15 17:11:59 +0100

    add history model

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -15,6 +15,7 @@ class Message(models.Model):
     )
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False)
 
     def __str__(self):
         return f"from {self.sender.first_name} to {self.receiver.first_name} at {self.timestamp}"
@@ -29,6 +30,21 @@ class Notification(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE)
     is_read = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return f"Notification for {self.user} about {self.message.id}"
+
+
+class MessageHistory(models.Model):
+    message = models.ForeignKey(
+        Message,
+        related_name="history",
+        on_delete=models.CASCADE,
+    )
+    old_content = models.TextField()
+    edited_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    edited_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"History for message {self.message.id} at {self.edited_at}"
+    

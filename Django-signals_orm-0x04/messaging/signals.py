afa2a5064a010b6ad79ab62eb49fc9040f9dfd21
Yusuff Abdulhakeem <hakeemyusuff19@gmail.com>
2025-06-15 17:17:05 +0100

    create signal to create message history

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,11 +1,25 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
 
 @receiver(post_save, sender=Message)
 def create_notification(sender, instance, created, **keargs):
     if created:
         Notification.objects.create(
             user=instance.receiver, 
-            message=instance
+            message=instance,
         )
+        
+@receiver(pre_save, sender=Message)
+def create_message_history(sender, instance, **kwargs):
+    if instance.id:
+        try:
+            old_msg = Message.objects.get(id=instance.id)
+            if old_msg.content != instance.content:
+                MessageHistory.objects.create(
+                    message=old_msg,
+                    old_content=old_msg.content,
+                )
+                instance.edited = True
+        except Message.DoesNotExist:
+            pass
